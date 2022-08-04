ba1981b4136ec85037cb07d2b886a30da5927f18
railstesh <railsriteshc56@gmail.com>
2022-08-04 19:19:18 +0530

    change for filter condition

--- frontend/src/components/availableJobs/ApplyFilter.js
+++ frontend/src/components/availableJobs/ApplyFilter.js
@@ -3,66 +3,59 @@ import { job_type, cultural_values } from '../../constJobList';
 import {
   Grid,
   Checkbox,
-  FormGroup,
   FormControlLabel,
   Typography,
+  Box
  } from '@mui/material';
 
 const ApplyFilter = ({filters, handleFilters}) => {
   return (
     <>
     <Grid container spacing={2} mt={1} mb={1} p={2} bgcolor={'#e7ebf0'} columns={12}>
-      <Grid item xs={6}>
-        <Grid container columns={12}>
-          <Grid item xs={12}>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              to apply filters for job-type:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormGroup>
-              { job_type.map(d => (
-                  <FormControlLabel
-                    label={d}
-                    control={
-                      <Checkbox
-                        data-test-id='job-type'
-                        checked={filters?.includes(d)}
-                        onChange={() => handleFilters(d)}
-                      />
-                    }
-                  />
-                ))
-              }
-            </FormGroup>
-          </Grid>
-        </Grid>
+      <Grid item xs={12}>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          to apply filters for job-type:
+        </Typography>
       </Grid>
-      <Grid item xs={6}>
-        <Grid container columns={12}>
-          <Grid item xs={12}>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              to apply filters for cultural values:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormGroup>
-              { cultural_values.map(d => (
-                  <FormControlLabel
-                    label={d}
-                    control={
-                      <Checkbox
-                        checked={filters?.includes(d)}
-                        onChange={() => handleFilters(d)}
-                      />
-                    }
-                  />
-                ))
+      <Box display={'flex'} width={'30%'}>
+        { job_type.map((d, i) => (
+          <><Grid item xs={12}>
+            <FormControlLabel
+              label={d}
+              control={
+                <Checkbox
+                  data-test-id='job-type'
+                  checked={filters?.includes(d)}
+                  onChange={() => handleFilters(d)}
+                />
               }
-            </FormGroup>
-          </Grid>
-        </Grid>
+            />
+          </Grid> <span style={{paddingTop: '12px', paddingRight: '8px'}}>{(i < 3) && 'or'}</span> </>
+          ))
+        }
+      </Box>
+      <Grid item xs={12}>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          to apply filters for job-type:
+        </Typography>
       </Grid>
+      <Box display={'flex'} width={'30%'}>
+        { cultural_values.map((d, i) => (
+          <><Grid item xs={12}>
+            <FormControlLabel
+              label={d}
+              control={
+                <Checkbox
+                  data-test-id='job-type'
+                  checked={filters?.includes(d)}
+                  onChange={() => handleFilters(d)}
+                />
+              }
+            />
+          </Grid> <span style={{paddingTop: '12px', paddingRight: '8px'}}>{(i < 3) && 'or'}</span></>
+          ))
+        }
+      </Box>
     </Grid>
     </>
   )
