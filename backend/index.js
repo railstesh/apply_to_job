346f8bf801bcf222195af8df7992a6bd452bb940
railstesh <railsriteshc56@gmail.com>
2022-08-04 18:06:58 +0530

    backend to storing data in database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,36 @@
-var http = require('http');
+const express = require("express");
+const cors = require("cors");
+const faunadb = require("faunadb");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080); 
+
+const {
+  Collection,
+  Create,
+} = faunadb.query;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = 8080;
+const FAUNA_SECRET = "fnAEtITQmxACScpf40LcpLa6ycSe9ceGAjRTmYzn";
+
+const client = new faunadb.Client({
+  secret: FAUNA_SECRET,
+});
+
+app.post("/applyJob", async (req, res) => {
+  try {
+    const { firstName, lastName, email, experience } = req.body;
+    const { data } = await client.query(
+      Create(Collection("users"), {
+        data: { firstName, lastName, email, experience },
+      })
+    );
+
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.description });
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
